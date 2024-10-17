87a4703990b130dca72d716d66bf34c0fa8eefc2
GuatavoSilva3164 <gustavosilva2008br@gmail.com>
2024-10-17 10:02:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const patients = ['Everson', 'Botelho', 'Rodolfo'];
+
+do {
+  let list = 'Lista de Pacientes:\n';
+  for (let i = 0; i < patients.length; i++) {
+    list += `${i + 1}º ${patients[i]}\n`;
+  }
+
+  const option = prompt(`${list}\nEscolha uma opção:\n1. Novo paciente\n2. Consultar paciente\n3. Sair`);
+
+  if (option === '1') {
+
+    const newPatient = prompt('Digite o nome do novo paciente:');
+    if (newPatient) {
+      patients.push(newPatient);
+      alert(`${newPatient} foi adicionado à fila.`);
+    }
+  } else if (option === '2') {
+    if (patients.length > 0) {
+      const consultedPatient = patients.shift();
+      alert(`Paciente consultado: ${consultedPatient}`);
+    } else {
+      alert('Amigo não tem nenhum paciente na fila.');
+    }
+  } else if (option === '3') {
+    alert('O programa foi encerrado.');
+    break;
+  } else {
+    alert('escolha uma das 3 opções anteriormente mostrada');
+  }
+} while (true);
